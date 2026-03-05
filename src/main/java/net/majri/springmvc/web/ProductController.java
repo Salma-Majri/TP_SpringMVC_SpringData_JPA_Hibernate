50dcf880d764bd4cba4f05f3468592d4613e199f
Salma Majri <salmamajri7@gmail.com>
2026-03-05 01:16:40 +0000

    feat: formulaire d'ajout avec validation Spring Validation

--- src/main/java/net/majri/springmvc/web/ProductController.java
+++ src/main/java/net/majri/springmvc/web/ProductController.java
@@ -1,11 +1,14 @@
 package net.majri.springmvc.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.majri.springmvc.entities.Product;
 import net.majri.springmvc.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -22,9 +25,26 @@ public class ProductController {
         model.addAttribute("productList", products);
         return "products";
     }
+    @GetMapping("/")
+    public String home(Model model) {
+        return "redirect:/index";
+    }
     @GetMapping("/delete")
     public String delete(@RequestParam(name="id") Long id){
         productRepository.deleteById(id);
         return "redirect:/index";
     }
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/newProduct";
+    }
+
 }
