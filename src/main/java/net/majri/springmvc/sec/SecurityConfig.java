fe2766da523a3abccb8d693e53c73a45e842f0fd
Salma Majri <salmamajri7@gmail.com>
2026-03-05 02:31:13 +0000

    feat: sécurisation de l'application en utilisant les annotations

--- src/main/java/net/majri/springmvc/sec/SecurityConfig.java
+++ src/main/java/net/majri/springmvc/sec/SecurityConfig.java
@@ -1,8 +1,10 @@
 package net.majri.springmvc.sec;
 
+import jakarta.annotation.security.PermitAll;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -32,10 +35,10 @@ public class SecurityConfig {
      @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
          return  http
-                 .formLogin(fl->fl.loginPage("/login"))
+                 .formLogin(fl->fl.loginPage("/login").permitAll())
                  .csrf(Customizer.withDefaults())
-                 .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
-                 .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+//                 .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+//                 .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
                  .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
                  .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                  .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
--- src/main/java/net/majri/springmvc/web/ProductController.java
+++ src/main/java/net/majri/springmvc/web/ProductController.java
@@ -5,6 +5,8 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.majri.springmvc.entities.Product;
 import net.majri.springmvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,10 +19,11 @@ import java.util.List;
 @Controller
 @AllArgsConstructor
 public class ProductController {
-
+    @Autowired
     private ProductRepository productRepository;
 
     @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList", products);
@@ -31,17 +34,20 @@ public class ProductController {
         return "redirect:/user/index";
     }
     @PostMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
     public String delete(@RequestParam(name="id") Long id){
         productRepository.deleteById(id);
         return "redirect:/user/index";
     }
     @GetMapping("/admin/newProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public String newProduct(Model model) {
         model.addAttribute("product", new Product());
         return "new-product";
     }
 
     @PostMapping("/admin/saveProduct")
+    @PreAuthorize("hasRole('ADMIN')")
     public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()) return "new-product";
         productRepository.save(product);
